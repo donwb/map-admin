cc8d2c1758e4667c499393840d438b39ce23c3ec
Don Browning <donwb@dons-imac.turner.com>
2016-12-02 15:53:30 -0500

    This shit doesn't work b/c EF wont serialize

--- api/Api.cs
+++ api/Api.cs
@@ -1,5 +1,9 @@
 using System;
 using Nancy;
+using System.Collections.Generic;
+using System.Web.Http;
+using maplib;
+using Newtonsoft.Json;
 
 namespace NancyApplication
 {
@@ -9,6 +13,41 @@ namespace NancyApplication
         public ApiModule()
         {
             Get("/api/ping", args => "Pong");
+            Get("/api/allUserActions", args => AllUserActions(args));
         }
+
+        private object AllUserActions(dynamic o)
+        {
+            maplib.MapController controller = new maplib.MapController();
+
+            var users = controller.AllUserActions();
+            AllUserActionsResponse resp = new AllUserActionsResponse();
+            resp.Users = users;
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.S‌​erializerSettings.Re‌​ferenceLoopHandling = ReferenceLoopHandling.Ignore; 
+
+            var j = JsonConvert.SerializeObject(resp);
+            
+            Console.WriteLine(j);
+            
+            Useractions ua = new Useractions();
+            ua.Id = 1;
+            ua.Userid = 1;
+            ua.Actionsid = 2;
+            ua.Actiondate = DateTime.Now;
+            //ua.Actions = users[0].Actions;
+            //ua.User = users[0].User;
+            List<Useractions> uas = new List<Useractions>();
+            uas.Add(ua);
+            AllUserActionsResponse r = new AllUserActionsResponse{Users = uas};
+
+            return Response.AsJson(r, HttpStatusCode.OK);
+
+        }
+
+    }
+
+    public class AllUserActionsResponse
+    {
+        public List<Useractions> Users {get; set;}
     } 
 }
--- api/Startup.cs
+++ api/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Nancy.Owin;
 
@@ -13,8 +14,27 @@ namespace NancyApplication
         public void Configure(IApplicationBuilder app)
         {
             
+            var dbVars = CheckEnvVars();
+            if(dbVars.Item1 == null || dbVars.Item2 == null) {
+                Console.WriteLine("Environment vars not set, stopping....");
+                Environment.Exit(1);
+            } else {
+                Console.WriteLine("User: " + dbVars.Item1 + " Pass:" + dbVars.Item2);
+            }
+            
+
             app.UseOwin(x => x.UseNancy());
             
+
+        }
+
+        private Tuple<string, string> CheckEnvVars() 
+        {
+            var user = Environment.GetEnvironmentVariable("DB_USER");
+            var pass = Environment.GetEnvironmentVariable("DB_PASSWORD");
+            
+            return new Tuple<string, string>(user, pass);
+
         }
 
    }
